5db966dfdefcec43fe5612123f4a93a87e55b54b
unknown <nikhilsaini735510@gmail.com>
2026-06-06 12:48:53 +0530

    Built TopicAnalyzerAgent

---
+++ LLM/groq_client.py
@@ -0,0 +1,9 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="meta-llama/llama-4-scout-17b-16e-instruct",
+    temperature=0,
+)
---
+++ Schemas/TopicAnalysisSchema.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import  TypedDict, Annotated, Optional, Literal, List
+
+class TopicAnalysisSchema(BaseModel):
+    topic: str = Field(description="Write down the Exact Topic")
+    domain: str = Field(description="Write down the Domain to which the given topic belongs")
+    research_type: Literal["Exploratory","Comparative","Historical","Technical","Predictive","Business","Policy"] = Field(description="The type of research that can be conducted on the given topic (Exploratory, Comparative, Historical, Technical, Predictive, Business, Policy).")
+    difficulty:  Literal["Easy", "Intermediate", "Hard"] = Field(description="The overall difficulty in researching on the given topic (Easy, Intermedate, Hard).")
+    subtopics: List[str] = Field(min_length=3,max_length=10,description="List all the relevant subtopics related in and around the given topic.")
+    keywords: List[str] = Field(min_length=3,max_length=15,description="List all the relevant keywords related in and around the given topic.")
+    research_goals: List[str] = Field(description="List all the research goals which can be achieved by doing research on the topic.")
